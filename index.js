4f89acd7f5cc616e9ea6957868fcd1e6b94fb56d
nguyenhung <mjsakju@gmail.com>
2018-08-30 16:09:17 +0900

    ok

--- index.js
+++ index.js
@@ -27,28 +27,31 @@ app.get('/download', function (req, res) {
 
 app.get('/image', function (req, res) {
     var url = req.query.url;
-    var type = req.query.type;
     var filename = "";
-    if (cacheImg[url + type] != undefined) {
-        filename = cacheImg[url + type].filename;
+    if (cacheImg[url] != undefined) {
+        filename = cacheImg[url].filename;
+        var filePath = __dirname + "/imgs/" + filename;
         return res.sendFile(__dirname + "/imgs/" + filename)
     }
-    filename = makeFilename() + "." + type;
-    download(url, filename, function () {
-        var filePath = __dirname + "/imgs/" + filename;
-        var filesize = getFilesizeInBytes(filePath)
-        if (filesize < 1000) {
-            fs.unlinkSync(filePath);
-            res.sendFile(__dirname + "/noimage.png");
-        } else {
-            var cache = {};
-            cache.url = url;
-            cache.type = type;
-            cache.filename = filename;
-            cacheImg[url + type] = cache;
-            res.sendFile(filePath)
-        }
-    });
+    filename = makeFilename();
+    try {
+        download(url, filename, function (extension) {
+            var filePath = __dirname + "/imgs/" + filename + "." + extension;
+            var filesize = getFilesizeInBytes(filePath);
+            if (filesize < 1000) {
+                fs.unlinkSync(filePath);
+                res.sendFile(__dirname + "/noimage.png");
+            } else {
+                var cache = {};
+                cache.url = url;
+                cache.filename = filename;
+                cacheImg[url] = cache;
+                res.sendFile(filePath)
+            }
+        });
+    } catch (error) {
+
+    }
 })
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
@@ -69,11 +72,14 @@ function normalizePort(val) {
     return false;
 }
 
-function download(url, filename, callback, error) {
-    https.get(url, function(response) {
-        response.pipe(fs.createWriteStream(__dirname + "/imgs/" + filename))
-        .on('close', callback)
-        .on('error', function (e) { console.log(e) });
+function download(url, filename, callback) {
+    http.get(url, function (response) {
+        var extension = response.headers['content-type'].split('/')[1];
+        response.pipe(fs.createWriteStream(__dirname + "/imgs/" + filename + "." + extension))
+            .on('close', function () {
+                callback(extension);
+            })
+            .on('error', function (e) { console.log(e) });
     });
 };
 
